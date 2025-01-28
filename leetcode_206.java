f85688724081f0db56c88560e2fee28eb3901613
vivek <vivan100123@gmail.com>
2025-01-28 23:04:30 +0530

    leetcode 206 solved

---
+++ leetcode_206.java
@@ -0,0 +1,39 @@
+class listNode {
+    int val;
+    listNode next;
+    listNode() {}
+    listNode(int val) { this.val = val; }
+    listNode(int val, listNode next) { this.val = val; this.next = next; }
+}
+class Solution {
+    public listNode reverseList(listNode head) {
+        listNode prev = null;
+        listNode curr = head;
+        while (curr != null) {
+            listNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+}
+public class leetcode_206 {
+    public static void main(String[] args) {
+        listNode head = new listNode(1);
+        head.next = new listNode(2);
+        head.next.next = new listNode(3);
+        head.next.next.next = new listNode(4);
+        head.next.next.next.next = new listNode(5);
+
+        Solution solution = new Solution();
+        listNode reversedHead = solution.reverseList(head);
+
+        // Print the reversed list
+        listNode curr = reversedHead;
+        while (curr != null) {
+            System.out.print(curr.val + " ");
+            curr = curr.next;
+        }
+    }
+}
