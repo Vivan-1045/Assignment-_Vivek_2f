fe9e0044ab145645a8af33cf1833e5894fa791e1
vivek <vivan100123@gmail.com>
2025-02-05 17:54:45 +0530

    leetcode 104 solved

---
+++ leetcode_104.java
@@ -0,0 +1,26 @@
+public class leetcode_104 {
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    public static void main(String[] args) {
+
+    }
+    public int maxDepth(TreeNode root) {
+         if (root==null){
+             return 0;
+         }
+         int left = maxDepth(root.left);
+         int right = maxDepth(root.right);
+         return Math.max(left,right);
+    }
+}
