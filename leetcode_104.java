3e524f94d0104f88c9b79c2697943fe0b7a1e04d
vivek <vivan100123@gmail.com>
2025-02-05 17:57:14 +0530

    leetcode 104 solved

--- leetcode_104.java
+++ leetcode_104.java
@@ -21,6 +21,6 @@ public class leetcode_104 {
          }
          int left = maxDepth(root.left);
          int right = maxDepth(root.right);
-         return Math.max(left,right);
+         return Math.max(left,right)+1;
     }
 }
